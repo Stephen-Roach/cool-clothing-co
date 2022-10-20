3ed9eff411e6895d730cbad8885803b693297374
Stephen-Roach <stephenroach21@gmail.com>
2022-10-20 12:59:39 -0500

    adds the sign-in routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/Home';
 import NavBar from './routes/navigation/NavBar';
+import SignIn from './routes/sign-in/SignIn';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<NavBar />}>
         <Route index={true} element={<Home />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   );
