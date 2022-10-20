6fe617d6d800342f2b7f145c25e77737bcc30326
Stephen-Roach <stephenroach21@gmail.com>
2022-10-20 10:34:46 -0500

    sets up the navbar routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/Home';
+import NavBar from './routes/navigation/NavBar';
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<NavBar />}>
+        <Route index={true} element={<Home />} />
+      </Route>
     </Routes>
   );
 };
